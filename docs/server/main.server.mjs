0c574adf251bb39432874b3fd4236361c93adb9c
cjstepan <calvinstepan@gmail.com>
2024-09-09 13:01:33 -0500

    Removed Angular project in favor of single-page application

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-SSJOVIZX.mjs";import"./chunk-G3PFTMHY.mjs";import"./chunk-NDYDZJSS.mjs";export{a as default};
--- ml-demo/src/app/model/process.ts
+++
@@ -1,5 +0,0 @@
-export interface Process {
-    id: number;
-    name: string;
-    description: string;
-}
